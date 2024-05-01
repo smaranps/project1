2f18e5e6999b91fc04170e46387806addf752499
smaranps <smaranpinisetty@gmail.com>
2024-05-01 16:51:23 -0400

    intial commit

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [random,setRandom]=useState(0)
+
+  
+  function generateRandom(){
+
+   const randomNunber = Math.round(Math.random()*10);
+   
+   setRandom(randomNunber)
+
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Generate" onPress={generateRandom}></Button>
+      
+      <Text>{random}</Text>
       <StatusBar style="auto" />
     </View>
   );
